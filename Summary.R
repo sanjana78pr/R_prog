c49c68358917f9909c644f40bf3acd1afab777d7
sanjana78pr <sanjanaprakash780@gmail.com>
2026-07-31 22:05:52 +0530

    Added 2nd program(basic)

---
+++ Summary.R
@@ -0,0 +1,8 @@
+students <- data.frame(
+  Name       = c("Ananya","Rohith","Priya","Karan","Sneha"),
+  Marks      = c(82,67,91,45,76),
+  Attendance = c(90,75,60,88)
+)
+
+str(students)
+summary(students)
